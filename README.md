7d9f30b6359739cade3b0f611209f0356b0227fd
Dave Caruso <dave@davecode.net>
2022-08-18 21:29:51 -0400

    the code

---
+++ README.md
@@ -0,0 +1,9 @@
+# SQLite DB of all Stable Diffusion generated images
+
+Not too long ago, someone in the Stable Diffusion Discord server shared a text file of every prompt and a list of links of the generated images. This repository contains code to convert that file to a SQLite database. I am hosting a copy of the database ready for use [here]() (2.9gb)
+
+The converter is written using Node.js and better-sqlite3, and took 162 seconds to convert on my desktop.
+
+There is also an implementation with Bun, but it doesn't work for me due to a bug. Were it to work, it would run over 3x faster than the node implementation.
+
+To run, you need an extracted copy of [ImageLinks.zip](https://drive.google.com/file/d/14_CRrWMw20OSSd5ZA-7epXdWF3DlwM8g/view) (2.5gb). Then you can run `bun convert.ts` or `node convert.mjs`.
---
+++ convert.mjs
@@ -0,0 +1,103 @@
+import Database from "better-sqlite3";
+import { unlinkSync, existsSync, createReadStream } from "fs";
+
+const start = performance.now();
+
+if (existsSync("ImageLinks.db")) {
+  unlinkSync("ImageLinks.db");
+}
+if (existsSync("ImageLinks.db-journal")) {
+  unlinkSync("ImageLinks.db-journal");
+}
+
+// Bun has stream issues
+const db = new Database("ImageLinks.db", {
+  create: true,
+  readwrite: true,
+});
+
+db.exec(`CREATE TABLE IF NOT EXISTS prompts(id INT, prompt TEXT)`);
+db.exec(
+  `CREATE TABLE data(id INT, promptid INT, url TEXT, timestamp INT, height INT, width INT, cfgscale FLOAT, num INT, grid TINYINT, steps INT, seed INT)`
+);
+
+const insertPrompt = db.prepare(
+  "INSERT INTO prompts (id, prompt) VALUES (?, ?)"
+);
+const insertData = db.prepare(
+  "INSERT INTO data (id, promptid, url, height, width, cfgscale, num, grid, steps, seed, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+);
+
+let promptId = -1;
+let dataId = 0;
+
+let num = 0;
+let lastCheckPointAt = performance.now();
+
+const transation = db.transaction((current) => {
+  for (const line of current) {
+    // Checkpoint
+    num++;
+    if (num % 100000 === 0) {
+      const now = performance.now();
+      console.log(
+        "100000 lines took " +
+          (now - lastCheckPointAt).toFixed(1) +
+          "ms" +
+          " at #" +
+          num
+      );
+      lastCheckPointAt = now;
+    }
+
+    if (line.startsWith("!")) {
+      promptId++;
+      if (promptId > 0) {
+        const prompt = line.slice(1).replace(/  +/g, " ").trim();
+        insertPrompt.run(promptId, prompt);
+      }
+    } else {
+      if (promptId > 0) {
+        dataId++;
+
+        insertData.run(
+          dataId,
+          promptId,
+          line,
+          Number(line.match(/(?:-H_)(\d+)/)?.[1] ?? 512),
+          Number(line.match(/(?:-W_)(\d+)/)?.[1] ?? 512),
+          Number(line.match(/(?:-C_)([\d\.]+)/)?.[1] ?? 7),
+          Number(line.match(/(?:-n_)(\d+)/)?.[1] ?? 1),
+          line.includes("-g") ? 1 : 0,
+          Number(line.match(/(?:-s_)(\d+)/)?.[1] ?? 50),
+          Number(line.match(/(?:-S_)(\d+)/)?.[1] ?? 0),
+          Number(line.match(/(?:ts-)(\d+)/)?.[1] ?? -1)
+        );
+      }
+    }
+  }
+});
+
+const decoder = new TextDecoder();
+
+let carry = "";
+
+const stream = createReadStream("ImageLinks.txt");
+stream.on("data", (data) => {
+  const current = (carry + decoder.decode(data)).replace(/\r/g, "").split("\n");
+  carry = current.pop();
+
+  transation(current);
+});
+
+stream.on("end", () => {
+  if (carry.length > 0) {
+    transation([carry]);
+  }
+
+  const end = performance.now();
+
+  console.log(
+    "Entire operation took " + ((end - start) / 1000).toFixed(2) + " seconds"
+  );
+});
